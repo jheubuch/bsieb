8bf86e956d228de374a817b9971ee4dc2867e368
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-03-19 18:34:10 +0100

    added more testing to main()

--- src/kernel/Kernel.java
+++ src/kernel/Kernel.java
@@ -12,22 +12,35 @@ public class Kernel {
         hund.setWelpe(hund2);
 
         Output.initScreen();
-        Output.setColor(Color.RED, Color.BLACK);
 
+        Output.setColor(Color.RED, Color.BLACK);
+        Output.setCursor(1, 1);
         Output.print("PRINTING String: ");
         Output.println("Thats a string");
         Output.println();
 
+        Output.setColor(Color.GREEN, Color.BLUE);
+        Output.setCursor(2, 3);
         Output.print("PRINTING char: ");
         Output.println('x');
         Output.println();
 
-        Output.print("PRINTING int: ");
+        Output.setColor(Color.BLACK, Color.TURQOISE);
+        Output.print("PRINTING positive int: ");
         Output.println(42);
+        Output.print("PRINTING negative int: ");
+        Output.println(-42);
+        Output.print("PRINTING zero int: ");
+        Output.println(0);
         Output.println();
 
-        Output.print("PRINTING long: ");
+        Output.setColor(Color.BLUE, Color.BROWN);
+        Output.print("PRINTING positive long: ");
         Output.println(4711L);
+        Output.print("PRINTING negative long: ");
+        Output.println(-4711L);
+        Output.print("PRINTING zero long: ");
+        Output.println(0L);
         Output.println();
     }
 }
