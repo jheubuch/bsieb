224dbbed04bcb4fc6938772ec9e1b7785c214ff1
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-04-07 21:06:05 +0200

    added switch to real mode in BIOS

--- src/rte/BIOS.java
+++ src/rte/BIOS.java
@@ -1,5 +1,7 @@
 package rte;
 
+import kernel.interrupt.Interrupt;
+
 public class BIOS {
   private final static int BIOS_MEMORY = 0x60000;
   private final static int BIOS_STKEND = BIOS_MEMORY+0x1000;
@@ -163,7 +165,11 @@ public class BIOS {
     MAGIC.wMem8(BIOS_MEMORY+61, (byte)inter); //set interrupt number
     MAGIC.inline(0x9C); //pushf
     MAGIC.inline(0xFA); //cli
-    //TODO lidtRM(); //load idt with real mode interrupt table
+
+    long tmp = (((long) 0) << 16) | (long) 1023;
+    MAGIC.inline(0x0F, 0x01, 0x5D);
+    MAGIC.inlineOffset(1, tmp);
+
     //call 16 bit code
     MAGIC.inline(0x56); //push e/rsi
     MAGIC.inline(0x57); //push e/rdi
@@ -183,7 +189,9 @@ public class BIOS {
     }
     MAGIC.inline(0x5F); //pop e/rdi
     MAGIC.inline(0x5E); //pop e/rsi
-    //TODO lidt(); //load idt with protected/long mode interrupt table
+
+    Interrupt.initIdt();
+
     MAGIC.inline(0x9D); //popf
   }
 }
