bef389b53bc22c88e964d0a3f39a1f3c94ce5d42
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-04-21 15:56:44 +0200

    improved run script for just running QEMU when compiling succeeded

--- _build-and-run.bat
+++ _build-and-run.bat
@@ -1,2 +1,2 @@
 java -jar "C:\sjc\sjc.jar" src/ -o boot
-qemu-system-i386 -boot a -fda BOOT_FLP.IMG -L "C:\Program Files\qemu"
+IF %ERRORLEVEL% EQU 0 qemu-system-i386 -boot a -fda BOOT_FLP.IMG -L "C:\Program Files\qemu"
